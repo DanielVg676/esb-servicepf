d1463324150768a529e786f90a241bfff9562cbf
Daniel Villarreal <156235653+DanielVg676@users.noreply.github.com>
2025-04-09 23:20:21 -0600

    Update CreateOrderRequest.java

--- src/main/java/com/utd/ti/soa/esb_service/model/CreateOrderRequest.java
+++ src/main/java/com/utd/ti/soa/esb_service/model/CreateOrderRequest.java
@@ -1,40 +1,20 @@
 package com.utd.ti.soa.esb_service.model;
 
-public class CreateOrderRequest {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateOrderRequest {
     private double amount;
     private String currency;
     private int userId;
 
-    // Constructor
     public CreateOrderRequest(double amount, String currency, int userId) {
         this.amount = amount;
         this.currency = currency;
-        this.userId = userId;
-    }
-
-    // Getters and Setters
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
         this.userId = userId;
     }
 }
